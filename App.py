aa89c91ad40895b2396f6e1683e2a991b7913e0c
Witchaz <Jengkug23@gmail.com>
2023-11-04 22:25:37 +0700

    call dialogflow and respond with tts

--- App.py
+++ App.py
@@ -1,17 +1,83 @@
 from tkinter import *
 from tkinter import ttk,filedialog,messagebox
 import tkinter as tk
-import threading
+import threading,playsound
 import speech_recognition as sr
 import os,time
+from google.cloud import dialogflow
+import constants
 
 def voice_Input():
+    def tts(text):
+        from google.cloud import texttospeech
+
+        client = texttospeech.TextToSpeechClient()
+
+        synthesis_input = texttospeech.SynthesisInput(text=text)
+
+        voice = texttospeech.VoiceSelectionParams(
+            language_code="th-Thai", ssml_gender=texttospeech.SsmlVoiceGender.NEUTRAL
+        )
+
+        audio_config = texttospeech.AudioConfig(
+            audio_encoding=texttospeech.AudioEncoding.MP3
+        )
+
+
+        response = client.synthesize_speech(
+            input=synthesis_input, voice=voice, audio_config=audio_config
+        )
+
+
+        with open("speak.mp3", "wb") as out:
+            out.write(response.audio_content)
+            print('Audio content written to file "speak.mp3"')
+        try:
+            playsound.playsound('speak.mp3')
+            os.remove("speak.mp3")
+        except FileNotFoundError:
+            print("File not found")
+
+    def detect_intent_texts(project_id, session_id, text, language_code):
+
+        
+
+        session_client = dialogflow.SessionsClient()
+
+        session = session_client.session_path(project_id, session_id)
+        print("Session path: {}\n".format(session))
+
+    
+        text_input = dialogflow.TextInput(text=text, language_code=language_code)
+
+        query_input = dialogflow.QueryInput(text=text_input)
+
+        response = session_client.detect_intent(
+            request={"session": session, "query_input": query_input}
+        )
+
+        print("=" * 20)
+        print("Query text: {}".format(response.query_result.query_text))
+        print(
+            "Detected intent: {} (confidence: {})\n".format(
+                response.query_result.intent.display_name,
+                response.query_result.intent_detection_confidence,
+            )
+        )
+        print("Fulfillment text: {}\n".format(response.query_result.fulfillment_text))
+        return format(response.query_result.fulfillment_text);
+
+
     vc = sr.Recognizer()
     def main(audio):#รับฟัง
         try:
             say = vc.recognize_google(audio,language="th-TH")
-            print(say)
+            print(say + "\n\n")
             text.config(text = say)
+            respond = detect_intent_texts(constants.PROJECT_ID,constants.SESSION_ID,say,constants.LANGUAGE_CODE)
+            tts(respond)
+            
+
         except sr.UnknownValueError:
             print("ไม่เข้าใจ")
         except sr.RequestError as e:
---
+++ constants.py
@@ -0,0 +1,4 @@
+
+PROJECT_ID = "smart-farm-gvas"
+SESSION_ID = "1234"
+LANGUAGE_CODE = "th-Thai"
